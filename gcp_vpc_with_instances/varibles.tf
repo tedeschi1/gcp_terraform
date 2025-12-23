bcd1ec8ae2b7debb9d0640aabcaa7bd9825b7bc6
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2025-12-23 16:10:23 -0600

    Variables for GCP project ID and credentials

--- gcp_vpc_with_instances/varibles.tf
+++ gcp_vpc_with_instances/varibles.tf
@@ -7,3 +7,8 @@ variable "gcp_credentials_file" {
   type = string
   default = "C:/Users/jdoe/Documents/csp-gcp-jdoe-1234.json"
 }
+
+variable "gcp_region" {
+  type = string
+  default = "us-central1"
+}
