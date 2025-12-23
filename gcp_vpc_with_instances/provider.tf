9d9bbe9fd9c87a2478fa060f7a5c529b168f80fd
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2025-12-23 15:57:03 -0600

    GCP provider configuration to provider.tf

---
+++ gcp_vpc_with_instances/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.14.1"
+    }
+  }
+}
+
+#GCP Provider Configuration
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+  credentials = file(var.gcp_credentials_file)
+}
