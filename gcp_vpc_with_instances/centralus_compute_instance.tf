bba4bbcd429166a9f118e9bade5883ab24ee64f9
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2025-12-23 15:53:36 -0600

    Google Compute Instance in central US region

---
+++ gcp_vpc_with_instances/centralus_compute_instance.tf
@@ -0,0 +1,48 @@
+# resource "google_service_account" "default" {
+#   account_id   = "my-custom-sa"
+#   display_name = "Custom SA for VM Instance"
+# }
+
+resource "google_compute_instance" "centralus" {
+  name         = "centralus-compute-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["centralus", "allow-ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = google_compute_network.vpc1_uscentral1.name
+    subnetwork = google_compute_subnetwork.vpc1_uscentral1_subnet1.name
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+#   service_account {
+#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+#     email  = google_service_account.default.email
+#     scopes = ["cloud-platform"]
+#   }
+  depends_on = [ google_compute_subnetwork.vpc1_uscentral1_subnet1 ]
+}
