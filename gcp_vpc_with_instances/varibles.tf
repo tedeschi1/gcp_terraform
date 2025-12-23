a257eb85bc2d64a7d9056b19748d6b6b94665d6e
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2025-12-23 15:58:55 -0600

    Variables for GCP project ID and credentials

---
+++ gcp_vpc_with_instances/varibles.tf
@@ -0,0 +1,9 @@
+variable "gcp_project_id" {
+  type = string
+  default = "csp-gcp-jdoe"
+}
+
+variable "gcp_credentials_file" {
+  type = string
+  default = "C:/Users/jdoe/Documents/csp-gcp-jdoe-1234.json"
+}
